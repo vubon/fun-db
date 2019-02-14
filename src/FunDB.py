8160422a5006ef09f5ae8232a4a5d7a892b0fdcc
vubon <vubon.roy@gmail.com>
2019-02-14 18:40:19 +0600

    data does not save

--- src/FunDB.py
+++ src/FunDB.py
@@ -4,18 +4,32 @@ import json
 
 class FunDB(object):
 
-    # def __init__(self, location):
-    #     self.location = os.path.expanduser(location)
-    #     self.load = self.location
+    def __init__(self, location):
+        self.location = os.path.expanduser(location)
+        self.load(self.location)
 
-    def set(self, **kwargs):
+    def load(self, location):
+        if os.path.exists(location):
+            self._load()
+
+    def _load(self):
+        self.db = json.loads(open(self.location, "r"))
+
+    def set(self, key, value):
         """
-        :param kwargs:
+        :param key:
+        :param value:
         :return:
         """
-        if isinstance(kwargs, dict):
-            pass
-        else:
-            raise ValueError(f"Set value should dictionary{self}")
+        try:
+            self.db[str(key).lower()] = value
+            self.dump_db()
+        except Exception as err:
+            print(err)
 
+    def dump_db(self):
+        try:
+            json.dump(self.db, open(self.location, "w+"))
 
+        except Exception as err:
+            print(f"Dump error {err}")
